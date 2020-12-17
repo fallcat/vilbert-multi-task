c3f6202ed1d37d6ab7f343ba47328c360ceb55c6
fallcat <rachel_you@outlook.com>
2020-12-17 13:44:28 -0500

    debug index out of range for zero shot dataset eval

--- vilbert/datasets/zero_shot_cls_dataset.py
+++ vilbert/datasets/zero_shot_cls_dataset.py
@@ -74,6 +74,8 @@ class ZeroShotClsDatasetVal(Dataset):
             _load_annotationsVal(
             annotations_jsonpath, task
         )
+        print(f'num images: {self._image_entries}')
+        print(f'num captions: {self._caption_entries_unique}')
         self._image_features_reader = image_features_reader
         self._tokenizer = tokenizer
 
@@ -167,29 +169,12 @@ class ZeroShotClsDatasetVal(Dataset):
     def __getitem__(self, index):
 
         # we iterate through every caption here.
-        image_item = self._image_entries
         image_idx = index
         image_id = self._image_entries[image_idx]
-        # image_idx = index % 2
 
-        # print("self._image_entries", len(self._image_entries))
-        image_entries = self._image_entries[image_idx:image_idx+1]
-        # print("image_entries", image_entries)
         features_all = self.features_all[image_idx:image_idx+1]
         spatials_all = self.spatials_all[image_idx:image_idx+1]
         image_mask_all = self.image_mask_all[image_idx:image_idx+1]
-
-        # if image_idx == 0:
-        #     image_entries = self._image_entries[:500]
-        #     features_all = self.features_all[:500]
-        #     spatials_all = self.spatials_all[:500]
-        #     image_mask_all = self.image_mask_all[:500]
-        #
-        # else:
-        #     image_entries = self._image_entries[500:]
-        #     features_all = self.features_all[500:]
-        #     spatials_all = self.spatials_all[500:]
-        #     image_mask_all = self.image_mask_all[500:]
 
         captions = []
         input_masks = []
@@ -198,25 +183,13 @@ class ZeroShotClsDatasetVal(Dataset):
         caption_idxs = list(range(len(self._caption_entries_unique)))
         for i, entry in enumerate(self._caption_entries_unique):
             caption = entry["token"]
-            # print("caption", caption.shape)
             input_mask = entry["input_mask"]
-            # print("input_mask", input_mask.shape)
             segment_ids = entry["segment_ids"]
-            # print("segment_ids", segment_ids.shape)
             captions.append(caption)
             input_masks.append(input_mask)
             segment_idss.append(segment_ids)
             if entry["class_label"] == self._image_classes[image_id]:
                 target_all[i] = 1
-
-        # print("image_id", image_id)
-        # print("target_all", target_all)
-        # print("target_all sum", sum(target_all))
-
-        # target_all = torch.zeros(500)
-        # for i, image_id in enumerate(image_entries):
-        #     if image_id == entry["image_id"]:
-        #         target_all[i] = 1
 
         return (
             features_all,
